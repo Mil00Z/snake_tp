95a83c830f140cc79ca148ae1417efac274998d2
Benji <matyka.benjamin@yahoo.fr>
2019-02-06 16:29:55 +0100

    snake V1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,110 @@
 $(document).ready(function(){
 
-// functions
+// Functions
 function mooving_snake(){
 		// console.log('je bouge !');
 		// console.warn('More mooving');
 }
 
-let snake = $('#snake');
+ // Objets du DOM & Carac
+const snake = $('#snake');
 let items_pop =  $('.items_pop');
+
+let snake_width = snake.css('width');
+console.log(snake_width);
+
 
 		// JS Way
 		// let root = document.documentElement;
 		// root.style.setProperty('--x','100%');
 
 
-		//Faire déplacer le Snake au Clavier
+// Borner le déplacement
+	const area = $('#area');
+	let  area_top = area.offset().top;
+	let area_left = area.offset().left;
+			 console.log('top = ' + area_top + '// left = ' + area_left);
+
+
+		//Déclencher event au clavier
 		$(document).on('keydown',function(e){
 
 				let the_key = e.which;
-				console.log(e.which);
+					// attention on récupère un int et pas une 'string-int'
 
+				//les datas get en css sont des string, conversion obligée
+				var posX = parseInt(snake.css('--x'),10);
+				var posY = parseInt(snake.css('--y'),10);
+
+
+				// tester ajout de position de pas fixe
+					var new_pos = 20;
+
+// Tester les cas d'appuis clavier
 				switch(the_key){
 
-					case '37':
-					snake.toggleClass('to-left');
-					break;
+						case 37:
+						posX = posX - new_pos;
+						console.log("Nouvelle position : " + posX);
+						snake.css('--x',posX).toggleClass('to-left');
+						break;
 
-					case '38':
-					snake.toggleClass('to-right');
-					break;
+						case 38:
+						posY = posY - new_pos;
+						console.log("Nouvelle position : " + posY);
+						snake.css('--y',posY).toggleClass('to-up');
+						break;
 
-					case '39':
-					snake.toggleClass('to-up');
-					break;
+						case 39:
+						posX = posX + new_pos;
+						console.log("Nouvelle position : " + posX);
+						snake.css('--x',posX).toggleCLass('to-right');
+						break;
 
-					case '40':
-					snake.toggleClass('to-bottom');
-					break;
+						case 40:
+						posY = posY + new_pos;
+						console.log("Nouvelle position : " + posY);
+						snake.css('--y',posY).toggleClass('to-bottom');
+						break;
 
-					default:
-					console.warn('press the direction key mother fucke !');
+						case 8:
+						$('body').append("<div class='debeug'>hello</div>");
+						break;
+
+						default:
+						console.warn('press the direction key mother fucker !');
 
 				}
 
-		});
+
+// Borner le cadre de jeux
+				items_pop.toggleClass('to-eat');
+
+				if ( posY > area_top || posX > area_left ){
+					// snake.fadeOut(800);
+					console.warn('The Snake was a Pimp');
+				} else {
+
+				}
+
+	// Si Snake touche un item
+				items_pop.each(function(){
+
+					var items_posY = $(this).offset().top;
+					var items_posX = $(this).offset().left;
+
+					console.log(items_posY + ' // ' + items_posX);
+
+							if (posY === items_posY || posX === items_posX){
+									$(this).remove();
+									snake.css('width',)
+						}
+
+			});
+
+});
+
+
 
 
 
